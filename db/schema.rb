84bce5c8de814ed1e5838eefb12dbff2e8e3156d
EC2 Default User <ec2-user@ip-172-31-76-8.ec2.internal>
2021-02-01 12:24:09 +0000

    bookers2

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_26_153916) do
+ActiveRecord::Schema.define(version: 2021_02_01_121803) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -26,11 +26,11 @@ ActiveRecord::Schema.define(version: 2021_01_26_153916) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "name"
     t.text "introduction"
+    t.string "profile_image_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "name"
-    t.string "profile_image_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
